09b58fea1b35cd405dfb4275658214f6c5faa107
Ubuntu <ubuntu@ip-172-31-14-159.us-west-2.compute.internal>
2021-06-18 07:43:57 +0000

    changing run_boolq_script

--- scripts/run_boolq_batch.sh
+++ scripts/run_boolq_batch.sh
@@ -2,8 +2,8 @@
 
 set -e
 set -x
-# model_name_arr=("bert-base-cased" "bert-large-cased" "albert-base-v2" "albert-large-v2" "albert-xlarge-v2" "roberta-base" "roberta-large")
-model_name_arr=("bert-large-cased" "roberta-base" "roberta-large")
+model_name_arr=("bert-base-cased" "albert-base-v2" "roberta-base" "roberta-large")
+# model_name_arr=("bert-large-cased" "roberta-base" "roberta-large")
 
 for(( i=0;i<${#model_name_arr[@]};i++)) do
 	bash run_boolq.sh ${model_name_arr[i]};
